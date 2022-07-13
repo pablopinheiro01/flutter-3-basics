ec80274ebb2d99f20ebfdf0512b7cc71d0275bee
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-07-13 20:01:28 -0300

    Adicionado tratamento de erro e mapeado novas mensagens de erro

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -14,96 +14,129 @@ class _FormScreenState extends State<FormScreen> {
   TextEditingController difficultyController = TextEditingController();
   TextEditingController imageController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Nova Tarefa'),
-      ),
-      body: Center(
-        child: Container(
-          height: 650,
-          width: 375,
-          decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(width: 3)
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: nameController,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Nome',
-                    fillColor: Colors.white70,
-                    filled: true,
-                  ),
-                ),
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Nova Tarefa'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              height: 650,
+              width: 375,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 3)
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: difficultyController,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Dificuldade',
-                    fillColor: Colors.white70,
-                    filled: true,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (String? value){
+                        if( value == null && value == "" ){
+                          return 'Insira um nome';
+                        }else{
+                          return null;
+                        }
+                      },
+                      controller: nameController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Nome',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  onChanged: (text){
-                    setState((){ //atualiza a tela mesmo sem a necessidade de passagem de parametro
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (String? value){
+                        if( value!.isEmpty || int.parse(value) > 5 || int.parse(value) < 1){
+                          return 'Insira uma dificuldade entre 1 e 5';
+                        }else{
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.number,
+                      controller: difficultyController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Dificuldade',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (value){
+                        if(value!.isEmpty){
+                          return 'Insira uma url pra imagem';
+                        }else{
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.url,
+                      onChanged: (text){
+                        setState((){ //atualiza a tela mesmo sem a necessidade de passagem de parametro
 
-                    });
-                  },
-                  controller: imageController,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Imagem',
-                    fillColor: Colors.white70,
-                    filled: true,
+                        });
+                      },
+                      controller: imageController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Imagem',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 72,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 2, color: Colors.blue)
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    //https://cdn.ome.lt/qnK3hf0_08-cjuoRDb98LkEg3Do=/1200x630/smart/extras/conteudos/darth-vader-fortnite.jpg
-                    imageController.text,
-                    fit: BoxFit.cover,
-                    errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
-                        return Image.asset('assets/images/nophoto.jpg', fit: BoxFit.cover,);
-                    },
-                  ),
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 2, color: Colors.blue)
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        //https://cdn.ome.lt/qnK3hf0_08-cjuoRDb98LkEg3Do=/1200x630/smart/extras/conteudos/darth-vader-fortnite.jpg
+                        imageController.text,
+                        fit: BoxFit.cover,
+                        errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
+                            return Image.asset('assets/images/nophoto.jpg', fit: BoxFit.cover,);
+                        },
+                      ),
 
-                ),
+                    ),
+                  ),
+                  ElevatedButton(onPressed: (){
+                    if(_formKey.currentState!.validate()){//valida o estado do formulario
+                      print(nameController.text);
+                      print( int.parse(difficultyController.text));
+                      print(imageController.text);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Salvando nova tarefa...'),),);
+                    }
+                  }, child: Text('Adicionar'))
+                ],
               ),
-              ElevatedButton(onPressed: (){
-                print(nameController.text);
-                print( int.parse(difficultyController.text));
-                print(imageController.text);
-              }, child: Text('Adicionar'))
-            ],
+            ),
           ),
         ),
       ),
